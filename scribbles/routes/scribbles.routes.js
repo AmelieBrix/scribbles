10833d3ca788a8ab2ef8f27eaf0e2e35dc7ef7b2
Aditya Raikar <raikaraditya22@gmail.com>
2024-07-18 13:42:19 +0200

    added routes for food-corner, art-fart, city-vibes, game-hib. And also added the file in app.js

--- scribbles/routes/scribbles.routes.js
+++ scribbles/routes/scribbles.routes.js
@@ -6,8 +6,16 @@ router.get("/scribbles/food-corner", (req, res, next) => {
   res.render("channels/food-corner");
 });
 
-router.get("/scribbles", (req, res, next) => {
-  res.render("scribbles")
+router.get("/scribbles/art-fart", (req, res, next) => {
+  res.render("channels/art-fart")
+})
+
+router.get("/scribbles/city-vibes", (req, res, next) => {
+  res.render("channels/city-vibes")
+})
+
+router.get("/scribbles/game-hub", (req, res, next) => {
+  res.render("channels/game-hub")
 })
 
 module.exports = router;
