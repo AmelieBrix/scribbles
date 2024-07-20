4f055c6470174b2cf2b98536f6caae0bfb901035
Aditya Raikar <raikaraditya22@gmail.com>
2024-07-20 13:17:25 +0200

    added something in scribbles.routes

--- scribbles/routes/scribbles.routes.js
+++ scribbles/routes/scribbles.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
     
+router.get("/", (req, res, next) => {
+  res.render("channels/food-corner");
+});
 
 /* GET home page */
 router.get("/scribbles/food-corner", (req, res, next) => {
