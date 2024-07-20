a963e26e8d8bdd07d772e288fd0abb9105d14cf4
Aditya Raikar <raikaraditya22@gmail.com>
2024-07-20 12:13:50 +0200

    added scribble model to scribbles.routes.js

--- scribbles/routes/scribbles.routes.js
+++ scribbles/routes/scribbles.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-    
+const Scribble = require('../models/Scribble.model')
 
 /* GET home page */
 router.get("/scribbles/food-corner", (req, res, next) => {
