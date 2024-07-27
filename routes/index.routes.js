5d443fd6ba87475a4b548597af49a88a7661fcba
Aditya Raikar <raikaraditya22@gmail.com>
2024-07-27 11:52:04 +0200

    added 2 nav bars

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index",{ isLoggedIn: req.session.currentUser ? true : false });
 });
 
 router.get("/scribbles", (req, res, next) => {
-  res.render("scribbles")
+  res.render("scribbles",{ isLoggedIn: req.session.currentUser ? true : false })
 })
 
 
