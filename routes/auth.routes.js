b6380f432e3faa6bdefbdf2aeddfa1fb8d039446
Aditya Raikar <raikaraditya22@gmail.com>
2024-08-01 22:41:14 +0200

    moves some reoutes to scribble.routes from auth.routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -33,7 +33,7 @@ router.get("/signup",isLoggedOut, (req, res, next) => {
         });
       })
       .then(userFromDB => {
-        res.redirect('/userProfile');   //replaced redirect with render
+        res.redirect('/userProfile');   
       })
       .catch(error => next(error));
   });
@@ -54,16 +54,15 @@ router.get("/signup",isLoggedOut, (req, res, next) => {
       const scribbles = await Scribble.find({ user: userId }).populate('user').populate('comments.user').exec();
       
       res.render('myscribbles', { 
-        scribbles, // Pass the array of scribbles
-        currentUserId: req.session.currentUser._id, // Pass the current logged-in user's ID
-        user: req.session.currentUser // Pass the current logged-in user
+        scribbles,
+        currentUserId: req.session.currentUser._id, 
+        user: req.session.currentUser 
       });
     } catch (error) {
       console.error(error);
       res.status(500).send('Internal server error');
     }
   });
-  
 
   router.get('/login',isLoggedOut, (req, res, next) => {
     res.render("auth/login")
@@ -102,193 +101,6 @@ router.get("/signup",isLoggedOut, (req, res, next) => {
     });
   });
 
-  router.get('/scribbles/create', isLoggedIn, (req, res) => {
-    res.render('auth/create-post',{user: req.session.currentUser})
-  })
-
-  router.post('/scribbles/create',fileUploader.single('ImageUrl'), async (req, res) => {
-    try {
-        const { title, category, description, location, comments } = req.body
-        const userId = req.session.currentUser._id;
-        const user = await User.findById(userId);
-        const ImageUrl = req.file ? req.file.path : '/images/default_post.png';
-          if (!user) {
-            return res.status(404).send('User not found');
-          }
-          const newScribble = new Scribble({
-            title,
-            category,
-            description,
-            location,
-            user: userId,
-            ImageUrl : ImageUrl
-          });
-          const categoryId = newScribble.category
-          await newScribble.save();
-        res.redirect(`/scribbles/${categoryId.replace(/\s+/g, '-').toLowerCase()}`)
-    } catch (error) {
-      res.render('scribbles', { errorMessage: 'Error creating scribble. Please try again.' });
-        console.log(error)
-    }
-  })
-
-  router.get('/scribbles/edit/:id', async (req, res) => {
-    try {
-      const scribbleId = req.params.id;
-      const scribble = await Scribble.findById(scribbleId);
-      if (!scribble) {
-        return res.status(404).send('Scribble not found');
-      }
-  
-      res.render('auth/edit-scribble', { scribble }); // Adjust view name as necessary
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Error retrieving scribble for editing');
-    }
-  });
-
-  router.post('/scribbles/edit/:id', fileUploader.single('ImageUrl'), async (req, res) => {
-    try {
-      const scribbleId = req.params.id;
-      const { title, category, description, location } = req.body;
-  
-      const updatedImageUrl = req.file ? req.file.path : req.body.ImageUrl;
-  
-      const updatedScribble = await Scribble.findByIdAndUpdate(
-        scribbleId,
-        {
-          title,
-          category,
-          description,
-          location,
-          ImageUrl: updatedImageUrl,
-        },
-        { new: true } 
-      );
-  
-      if (!updatedScribble) {
-        return res.status(404).send('Scribble not found');
-      }
-  
-      res.redirect(`/scribbles/${updatedScribble.category.replace(/\s+/g, '-').toLowerCase()}`);
-    } catch (error) {
-      console.error('Error updating scribble:', error);
-      res.render('edit-scribble', {
-        errorMessage: 'Error updating scribble. Please try again.',
-        scribble: req.body,
-      });
-    }
-  });
-
-  router.get('/scribbles', isLoggedIn, async (req, res, next) => {
-    try {
-      const posts = await Scribble.find()
-        .populate('user')
-        .populate({
-          path: 'comments',
-          populate: { path: 'user' }
-        });
-      res.render("scribbles", { posts });
-    } catch (err) {
-      next(err);
-    }
-  });
-
-  router.get('/scribbles/:id', isLoggedIn, async (req, res, next) => {
-    try {
-      const scribbleId = req.params.id;
-      const userId = req.session.currentUser._id;
-      const scribble = await Scribble.findById(scribbleId)
-      .populate('user')
-      .populate({
-        path: 'comments',
-        populate: { path: 'user' }
-      });
-      const userHasLiked = scribble.likes.includes(userId);
-      const isOwner = scribble.user._id.toString() === userId.toString();
-      res.render("channels/scribble", { 
-        scribble, isOwner: isOwner, 
-        currentUserId: userId, 
-        user: req.session.currentUser,
-        userHasLiked
-      });
-    } catch (err) {
-      next(err);
-    }
-  });
-
-  router.post('/scribbles/:id/comments', isLoggedIn, async (req, res) => {
-    try {
-      const { content } = req.body;
-      const scribbleId = req.params.id;
-      const userId = req.session.currentUser._id;
-  
-      const newComment = new Comment({
-        content,
-        user: userId,
-        scribble: scribbleId
-      });
-  
-      await newComment.save();
-  
-      const scribble = await Scribble.findById(scribbleId);
-      if (!scribble) {
-        return res.status(404).send('Scribble not found');
-      }
-  
-      scribble.comments.push(newComment._id);
-      await scribble.save();
-  
-      res.redirect(`/scribbles/${scribbleId}`);
-    } catch (error) {
-      res.render('scribbles', { errorMessage: 'Error adding comment. Please try again.' });
-      console.log(error);
-    }
-  });
-
-  router.get('/comments/delete/:id', isLoggedIn, async (req, res, next) => {
-    try {
-      const commentId = req.params.id;
-      const userId = req.session.currentUser._id;
-      const comment = await Comment.findById(commentId).populate('user');
-      const scribbleId = comment.scribble
-  
-      if (!comment) {
-        return res.status(404).send('Comment not found');
-      }
-  
-      if (comment.user._id.toString() !== userId.toString()) {
-        return res.status(403).send('Unauthorized to delete this comment');
-      }
-  
-      await Comment.findByIdAndDelete(commentId);
-      res.redirect(`/scribbles/${scribbleId}`);
-    } catch (err) {
-      next(err);
-    }
-  });
-
-  router.get('/scribbles/delete/:id', isLoggedIn, async (req, res, next) => {
-    try {
-      const scribbleId = req.params.id;
-      const userId = req.session.currentUser._id;
-      const scribble = await Scribble.findById(scribbleId);
-      const categoryId = scribble.category
-      if (!scribble) {
-        return res.status(404).send('Scribble not found');
-      }
-  
-      if (scribble.user.toString() !== userId.toString()) {
-        return res.status(403).send('Unauthorized to delete this scribble');
-      }
-  
-      await Scribble.findByIdAndDelete(scribbleId);
-      res.redirect(`/scribbles/${categoryId.replace(/\s+/g, '-').toLowerCase()}`); // replaces the link we use to query the database back to what we have in our url
-    } catch (err) {
-      next(err);
-    }
-  });
-
   router.get('/userProfile/edit', isLoggedIn, (req, res, next) => {
     res.render('auth/edit-profile', { user: req.session.currentUser });
   });
@@ -316,69 +128,19 @@ router.get("/signup",isLoggedOut, (req, res, next) => {
               return User.findByIdAndUpdate(userId, updateUser, { new: true });
           })
           .then(updatedUser => {
-              console.log(req.session.currentUser,"here")
-              req.session.currentUser = updatedUser; // Update session with new user info
+              req.session.currentUser = updatedUser; 
               res.render('auth/profile', { user: updatedUser });
           })
           .catch(error => next(error));
   } else {
       User.findByIdAndUpdate(userId, updateUser, { new: true })
           .then(updatedUser => {
-            console.log(req.session.currentUser,"here")
-              req.session.currentUser = updatedUser; // Update session with new user info
+              req.session.currentUser = updatedUser; 
               res.render('auth/profile', { user: updatedUser });
           })
           .catch(error => next(error));
   }
 });
-
-router.get('/scribbles/:id/like', isLoggedIn, (req, res, next) => {
-  const scribbleId = req.params.id;
-  res.redirect(`/scribbles/${scribbleId}`);
-});
-
-router.post('/scribbles/:id/like',isLoggedIn, async (req, res) => {
-  try {
-    const scribbleId = req.params.id;
-    const userId = req.session.currentUser._id;
-
-    const scribble = await Scribble.findById(scribbleId);
-
-    if (!scribble) {
-      return res.status(404).json({ success: false, message: 'Scribble not found' });
-    }
-
-    if (scribble.likes.includes(userId)) {
-      // If already liked, remove the like
-      scribble.likes.pull(userId);
-    } else {
-      // Otherwise, add the like
-      scribble.likes.push(userId);
-    }
-
-    await scribble.save();
-
-    res.json({ success: true, likes: scribble.likes });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Internal server error' });
-  }
-});
-
-router.get('/user/likes', isLoggedIn, async (req, res) => {
-  try {
-    const userId = req.session.currentUser._id;
-    const likedScribbles = await Scribble.find({ likes: userId });
-
-    res.render('auth/liked-posts', { likedScribbles, user: req.session.currentUser });
-  } catch (error) {
-    console.error(error);
-    res.status(500).render('error', { message: 'Internal server error' });
-  }
-});
-
-
-
 
   module.exports = router;
 
--- routes/scribbles.routes.js
+++ routes/scribbles.routes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const Scribble = require('../models/Scribble.model')
 const User = require('../models/User.model')
 const Comment = require('../models/Comment.model')
+const fileUploader = require('../config/cloudinary.config');
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard.js');
 
 const categories = ["food-corner", "art-fart", "city-vibes", "game-hub"]; // These need to match the urls
 
@@ -19,5 +21,255 @@ categories.forEach(category => {
   });
 });
 
+/* scribble creation */
+
+router.get('/scribbles/create', isLoggedIn, (req, res) => {
+  res.render('auth/create-post',{user: req.session.currentUser})
+})
+
+router.post('/scribbles/create',fileUploader.single('ImageUrl'), async (req, res) => {
+  try {
+      const { title, category, description, location, comments } = req.body
+      const userId = req.session.currentUser._id;
+      const user = await User.findById(userId);
+      const ImageUrl = req.file ? req.file.path : '/images/default_post.png';
+        if (!user) {
+          return res.status(404).send('User not found');
+        }
+        const newScribble = new Scribble({
+          title,
+          category,
+          description,
+          location,
+          user: userId,
+          ImageUrl : ImageUrl
+        });
+        const categoryId = newScribble.category
+        await newScribble.save();
+      res.redirect(`/scribbles/${categoryId.replace(/\s+/g, '-').toLowerCase()}`)
+  } catch (error) {
+    res.render('scribbles', { errorMessage: 'Error creating scribble. Please try again.' });
+      console.log(error)
+  }
+})
+
+/* scribble edit */
+
+router.get('/scribbles/edit/:id', async (req, res) => {
+  try {
+    const scribbleId = req.params.id;
+    const scribble = await Scribble.findById(scribbleId);
+    if (!scribble) {
+      return res.status(404).send('Scribble not found');
+    }
+
+    res.render('auth/edit-scribble', { scribble }); // Adjust view name as necessary
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error retrieving scribble for editing');
+  }
+});
+
+router.post('/scribbles/edit/:id', fileUploader.single('ImageUrl'), async (req, res) => {
+  try {
+    const scribbleId = req.params.id;
+    const { title, category, description, location } = req.body;
+
+    const updatedImageUrl = req.file ? req.file.path : req.body.ImageUrl;
+
+    const updatedScribble = await Scribble.findByIdAndUpdate(
+      scribbleId,
+      {
+        title,
+        category,
+        description,
+        location,
+        ImageUrl: updatedImageUrl,
+      },
+      { new: true } 
+    );
+
+    if (!updatedScribble) {
+      return res.status(404).send('Scribble not found');
+    }
+
+    res.redirect(`/scribbles/${updatedScribble.category.replace(/\s+/g, '-').toLowerCase()}`);
+  } catch (error) {
+    console.error('Error updating scribble:', error);
+    res.render('edit-scribble', {
+      errorMessage: 'Error updating scribble. Please try again.',
+      scribble: req.body,
+    });
+  }
+});
+
+/* getting scribbles */
+
+router.get('/scribbles', isLoggedIn, async (req, res, next) => {
+  try {
+    const posts = await Scribble.find()
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: { path: 'user' }
+      });
+    res.render("scribbles", { posts });
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* getting a individual scribble */
+
+router.get('/scribbles/:id', isLoggedIn, async (req, res, next) => {
+  try {
+    const scribbleId = req.params.id;
+    const userId = req.session.currentUser._id;
+    const scribble = await Scribble.findById(scribbleId)
+    .populate('user')
+    .populate({
+      path: 'comments',
+      populate: { path: 'user' }
+    });
+    const userHasLiked = scribble.likes.includes(userId);
+    const isOwner = scribble.user._id.toString() === userId.toString();
+    res.render("channels/scribble", { 
+      scribble, isOwner: isOwner, 
+      currentUserId: userId, 
+      user: req.session.currentUser,
+      userHasLiked
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+
+
+/* sending a comment */
+
+router.post('/scribbles/:id/comments', isLoggedIn, async (req, res) => {
+  try {
+    const { content } = req.body;
+    const scribbleId = req.params.id;
+    const userId = req.session.currentUser._id;
+
+    const newComment = new Comment({
+      content,
+      user: userId,
+      scribble: scribbleId
+    });
+
+    await newComment.save();
+
+    const scribble = await Scribble.findById(scribbleId);
+    if (!scribble) {
+      return res.status(404).send('Scribble not found');
+    }
+
+    scribble.comments.push(newComment._id);
+    await scribble.save();
+
+    res.redirect(`/scribbles/${scribbleId}`);
+  } catch (error) {
+    res.render('scribbles', { errorMessage: 'Error adding comment. Please try again.' });
+    console.log(error);
+  }
+});
+
+/* deleting a comment */
+
+router.get('/comments/delete/:id', isLoggedIn, async (req, res, next) => {
+  try {
+    const commentId = req.params.id;
+    const userId = req.session.currentUser._id;
+    const comment = await Comment.findById(commentId).populate('user');
+    const scribbleId = comment.scribble
+
+    if (!comment) {
+      return res.status(404).send('Comment not found');
+    }
+
+    if (comment.user._id.toString() !== userId.toString()) {
+      return res.status(403).send('Unauthorized to delete this comment');
+    }
+
+    await Comment.findByIdAndDelete(commentId);
+    res.redirect(`/scribbles/${scribbleId}`);
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* deleting a scribble */
+
+router.get('/scribbles/delete/:id', isLoggedIn, async (req, res, next) => {
+  try {
+    const scribbleId = req.params.id;
+    const userId = req.session.currentUser._id;
+    const scribble = await Scribble.findById(scribbleId);
+    const categoryId = scribble.category
+    if (!scribble) {
+      return res.status(404).send('Scribble not found');
+    }
+
+    if (scribble.user.toString() !== userId.toString()) {
+      return res.status(403).send('Unauthorized to delete this scribble');
+    }
+
+    await Scribble.findByIdAndDelete(scribbleId);
+    res.redirect(`/scribbles/${categoryId.replace(/\s+/g, '-').toLowerCase()}`); // replaces the link we use to query the database back to what we have in our url
+  } catch (err) {
+    next(err);
+  }
+});
+
+/* liking a scribble */
+
+router.get('/scribbles/:id/like', isLoggedIn, (req, res, next) => {
+  const scribbleId = req.params.id;
+  res.redirect(`/scribbles/${scribbleId}`);
+});
+
+router.post('/scribbles/:id/like',isLoggedIn, async (req, res) => {
+  try {
+    const scribbleId = req.params.id;
+    const userId = req.session.currentUser._id;
+
+    const scribble = await Scribble.findById(scribbleId);
+
+    if (!scribble) {
+      return res.status(404).json({ success: false, message: 'Scribble not found' });
+    }
+
+    if (scribble.likes.includes(userId)) {
+      // If already liked, remove the like
+      scribble.likes.pull(userId);
+    } else {
+      // Otherwise, add the like
+      scribble.likes.push(userId);
+    }
+
+    await scribble.save();
+
+    res.json({ success: true, likes: scribble.likes });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+/* getting the liked posts */
+
+router.get('/user/likes', isLoggedIn, async (req, res) => {
+  try {
+    const userId = req.session.currentUser._id;
+    const likedScribbles = await Scribble.find({ likes: userId });
+
+    res.render('auth/liked-posts', { likedScribbles, user: req.session.currentUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).render('error', { message: 'Internal server error' });
+  }
+});
 
 module.exports = router;
